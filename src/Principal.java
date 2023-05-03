47da2ddcfa6420629673f6a1c763a3a77c6d6922
Estudiante <Estudiante@DESKTOP-OK4U5CG>
2023-05-03 13:25:14 +0200

    Ampliacion de comentario inicial

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,10 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//Esta es la clase principal de mi proyecto de telecomunicaciones
+		//para la asignatura de ticII
+		
+		
 	}
 
 }
